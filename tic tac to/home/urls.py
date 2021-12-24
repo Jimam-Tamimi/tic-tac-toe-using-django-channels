22c447a5de01d8070dbbdc36979b974ae8140909
jimam <jimamtamimi12@gmail.com>
2021-12-24 18:54:35 +0600

    comit

--- tic tac to/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.urls.conf import include
-from .views import *
-
-urlpatterns = [
-    path('', home), 
-    path('play/<str:code>/<str:username>/', play), 
-]
